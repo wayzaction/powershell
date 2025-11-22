e327b2cc51191a87a3119fbb061147528e81068b
Wayz <106933617+wayzaction@users.noreply.github.com>
2025-11-22 12:54:28 +0000

    Remove unnecessary characters from prompts

--- Password Checker/passwordchecker.ps1
+++ Password Checker/passwordchecker.ps1
@@ -1,6 +1,6 @@
 cls
-$firstName = Read-Host "Name:"
-$lastName = Read-Host "Last Name:"
+$firstName = Read-Host "Name"
+$lastName = Read-Host "Last Name"
 $correctbirth = "^\d{2}\.\d{2}\.\d{4}$" 
 $correctpassword = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s]).{15,}$" #This monstrosity is a password criteria. 15 symbols, 1 capital letter and 1 special symbol. Powershell syntax sucks.
 
@@ -13,7 +13,7 @@ $birth = Read-Host "Date of Birth (dd.mm.yyyy)"
 until ($birth -match $correctbirth)
 
 #Ask user to create a password..
-do { $password = Read-Host "Think of a password:"  
+do { $password = Read-Host "Think of a password"  
    }
 
 #..until user follows the criteria.
