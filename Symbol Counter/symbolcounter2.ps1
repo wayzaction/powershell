8b7ae1bd98f8ab4446c5b9d52716323f5f685806
Wayz <106933617+wayzaction@users.noreply.github.com>
2025-11-26 13:19:17 +0200

    Readability improvements

--- Symbol Counter/symbolcounter2.ps1
+++ Symbol Counter/symbolcounter2.ps1
@@ -3,37 +3,26 @@ function Counter {
 [CmdletBinding()]
 
 param(
-
 [Parameter(ValueFromPipeline=$true)]
-
 $Text
-
      )
 
 
 BEGIN {
-
 $global:Letters = 0
 Write-Host "String".PadRight(10),"Length"
-
       }
 
 
 PROCESS {
-
 $global:Letters = $Text.Length
-
 Write-Host $Text.PadRight(10) $global:Letters
-
         }
 
 
 END {
-
 Write-Host "Done" -ForegroundColor Green
-
     }
-
                 }
 
 "Hollow", "Precise" | Counter
