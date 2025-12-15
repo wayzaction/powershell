3192c0cd3e30b0d76a86e4387fac806fbc77a5ec
Wayz <106933617+wayzaction@users.noreply.github.com>
2025-12-15 14:35:35 +0200

    Update UserImportCSV.ps1 for password handling and OU creation

--- Domain/UserImportCSV.ps1
+++ Domain/UserImportCSV.ps1
@@ -1,11 +1,15 @@
 $logFilePath = "C:\temp\log.txt"
-
+$Pass = Read-Host "Think of a temporary password for users in the CSV file" -AsSecureString
+$ErrorActionPreference = 'silentlycontinue'
+ 
 Import-Csv -Path C:\users.csv | ForEach-Object {
-
     $samAccountName = "$($_.FirstName)$($_.LastName)"
-    
+    $DomainSyntax = "DC=" + $_.Domain -replace "\.", ",DC="
+    $OuSyntax = "OU=" + $_.Unit + ("," + $DomainSyntax -replace "\.", ",DC=")
+
+    New-ADOrganizationalUnit -Name $_.Unit -Path $DomainSyntax -ProtectedFromAccidentalDeletion $False
     try {
-        New-ADUser -Name $_.FirstName -Enabled $true -GivenName $_.FirstName -Surname $_.LastName -AccountPassword (ConvertTo-SecureString $_.password -AsPlainText -Force) -SamAccountName $samAccountName -UserPrincipalName "$samAccountName@it.loc"
+        New-ADUser -Name $_.FirstName -Enabled $true -GivenName $_.FirstName -Surname $_.LastName -AccountPassword $Pass -Path $OuSyntax -SamAccountName $samAccountName -UserPrincipalName "$samAccountName@it.loc" -ChangePasswordAtLogon:$true
         "$samAccountName created successfully." | Out-File -FilePath $logFilePath -Append
     } catch {
         # Fail
@@ -16,9 +20,11 @@ Import-Csv -Path C:\users.csv | ForEach-Object {
 # CSV EXAMPLE
 
 <#
-FirstName,LastName,Password
-Hot,Dog,Kevade123
-Red,Cat,Kevade123
-Regular,Guy,Kevade123
-John,Doe,123
+
+FirstName,LastName,Unit,Domain
+Max,Wayz,Account,it.loc
+Red,Harlow,Account,it.loc
+John,Marston,Account,it.loc
+Yellow,White,Account,it.loc
+
 #>
