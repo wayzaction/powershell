aebef1df8c78af2f2c3446a6c50a276eb7f632f5
Wayz <106933617+wayzaction@users.noreply.github.com>
2025-12-15 03:24:33 +0200

    Add PowerShell script to install ADDS and DNS

---
+++ Domain/Forest.ps1
@@ -0,0 +1,12 @@
+$Domain = Read-Host "Think of a domain name"
+
+Write-Host "Installing ADDS and DNS tools.."
+Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+Install-WindowsFeature -Name DNS -IncludeManagementTools
+
+Import-Module ActiveDirectory
+Import-Module DnsServer
+
+Install-ADDSForest -DomainName "$Domain" -SafeModeAdministratorPassword (Read-Host -Prompt "Create Administrator password" -AsSecureString) -InstallDns -Force
+
+Restart-Computer
