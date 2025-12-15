3d01c15256cdf167df0c94d5927a11dbebeb0414
Wayz <106933617+wayzaction@users.noreply.github.com>
2025-12-15 04:39:03 +0200

    Add CSV example for user import
    Added a CSV example for user import.

---
+++ Domain/UserImportCSV.ps1
@@ -0,0 +1,24 @@
+$logFilePath = "C:\temp\log.txt"
+
+Import-Csv -Path C:\users.csv | ForEach-Object {
+
+    $samAccountName = "$($_.FirstName)$($_.LastName)"
+    
+    try {
+        New-ADUser -Name $_.FirstName -Enabled $true -GivenName $_.FirstName -Surname $_.LastName -AccountPassword (ConvertTo-SecureString $_.password -AsPlainText -Force) -SamAccountName $samAccountName -UserPrincipalName "$samAccountName@it.loc"
+        "$samAccountName created successfully." | Out-File -FilePath $logFilePath -Append
+    } catch {
+        # Fail
+        "$samAccountName failed to create." | Out-File -FilePath $logFilePath -Append
+    }
+}
+
+# CSV EXAMPLE
+
+#<
+FirstName,LastName,Password
+Hot,Dog,Kevade123
+Red,Cat,Kevade123
+Regular,Guy,Kevade123
+John,Doe,123
+>#
